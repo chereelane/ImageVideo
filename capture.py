f4e14913a858062119947d1ef1cb52f0ffbb09e8
Cheree <38049977+chereelane@users.noreply.github.com>
2021-12-30 13:32:32 -0500

    added quit button to video capture to break the script

--- capture.py
+++ capture.py
@@ -3,19 +3,28 @@ import cv2, time
 # Create a video object
 video = cv2.VideoCapture(0)
 
-check, frame = video.read()
+a = 1
 
-print(check)
-print(frame)
+# Reads and prints frame
+while True:
+    a = a + 1
+    check, frame = video.read()
 
-# Set time to hold for 3 seconds
-time.sleep(6)
-cv2.imshow("Capturing", frame)
+    print(check)
+    print(frame)
 
+    # Converts color frame to gray
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Show the gray scale images
+    cv2.imshow("Capturing", gray)
 
-# Press any button to close the window
+    # Press any button to close the window
+    key = cv2.waitKey(1)
 
-cv2.waitKey(0)
+    # stops script if 'q' is pressed
+    if key == ord('q'):
+        break
+print(a)
 # Release the camera/video
 video.release()
 # Destroys all open windows.
