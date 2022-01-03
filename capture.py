6779e5097181fe9995469f1c080ea555b0cb9723
Cheree <38049977+chereelane@users.noreply.github.com>
2022-01-02 19:33:30 -0500

    creating file to write script for plotting the graph, created a figure p and constructed graph q.

--- capture.py
+++ capture.py
@@ -38,6 +38,8 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
     status_list.append(status)
 
+    status_list = status_list[-2:]
+
     # Records the time that the status changes from 0 to 1
     if status_list[-1] == 1 and status_list[-2] == 0:
         times.append(datetime.now())
---
+++ plotting.py
@@ -0,0 +1,10 @@
+from capture import df
+
+from bokeh.plotting import figure, show, output_file
+
+p = figure(x_axis_type='datetime', height=100, width=500, responsive=True, title='Motion Detector Graph')
+
+q = p.quad(left=df["Start"], right=df["End"], bottom=0, top=1, color="green")
+
+output_file("Graph.html")
+show(p)
